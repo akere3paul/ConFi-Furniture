142e72f73268f6efa489d1393d0cae5dfda791cb
Akere G. Paul <akeregobari@gmail.com>
2025-02-01 01:02:56 +0100

    main

---
+++ static/productStorage.js
@@ -0,0 +1,30 @@
+
+function productStorage() {
+    
+    return [
+        {type: 'sofas', name: 'Coral Risk', price: 205000.00},
+        {type: 'sofas', name: 'Ocean Bliss', price: 290000.00},
+        {type: 'sofas', name: 'Blue Rays', price: 340000.00},
+        {type: 'sofas', name: 'Love Charm', price: 275000.00},
+        {type: 'sofas', name: 'Green Armour', price: 310000.00},
+        {type: 'sofas', name: "Benjamin's Best", price: 225000.00},
+        {type: 'sofas', name: 'Sea Party', price: 380000.00},
+        {type: 'sofas', name: 'Mamba Jamba', price: 260000.00},
+        {type: 'beds', name: 'Penny Flex', price: 420000.00},
+        {type: 'beds', name: 'Oxford Ovals', price: 450000.00},
+        {type: 'beds', name: 'Sharp Jade', price: 490000.00},
+        {type: 'beds', name: 'Tokyo Town', price: 505000.00},
+        {type: 'beds', name: 'Vast Venny', price: 530000.00},
+        {type: 'beds', name: 'Pink Drip', price: 485000.00},
+        {type: 'beds', name: 'Mysterious Den', price: 435000.00},
+        {type: 'beds', name: 'Forest Fence', price: 445000.00},
+        {type: 'chairs', name: 'Yuki Chooper', price: 215000.00},
+        {type: 'chairs', name: 'Incredible Yolk', price: 240000.00},
+        {type: 'chairs', name: 'Blue Whales', price: 275000.00},
+        {type: 'chairs', name: 'Lemon Lint', price: 53000.00},
+        {type: 'chairs', name: 'Workshop Bob', price: 42500.00},
+        {type: 'chairs', name: 'Raining Fire', price: 65500.00},
+        {type: 'chairs', name: 'Wooden Wander', price: 76500.00},
+        {type: 'chairs', name: "Bug's Beauty", price: 87500.00},
+    ]
+}
